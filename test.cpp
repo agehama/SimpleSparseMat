c58d3d5eeef3bc975d8c72fe0fedf6ad098809a3
agehama <agehama1@gmail.com>
2021-03-20 12:37:15 +0900

    initial commit

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/included/unit_test.hpp>
+
+#include "SimpleSparseMat.hpp"
+
+BOOST_AUTO_TEST_SUITE(aa)
+
+BOOST_AUTO_TEST_CASE(case1)
+{
+	BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
