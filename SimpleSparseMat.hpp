ceb222881bb692d29473012429a0c87c4d3f4a15
agehama <agehama1@gmail.com>
2021-03-21 10:32:27 +0900

    init CSR format

--- SimpleSparseMat.hpp
+++ SimpleSparseMat.hpp
@@ -1,5 +1,51 @@
 #pragma once
+#include <vector>
 
-class SimpleSparseMat
+namespace ssmat
 {
-};
+	using IndexT = std::int32_t;
+
+	template<typename T>
+	struct SparseEntry
+	{
+		SparseEntry(IndexT x, IndexT y, T v) :x(x), y(y), v(v) {}
+		IndexT x, y;
+		T v;
+	};
+
+	template<typename T>
+	class SparseMat
+	{
+	public:
+		SparseMat(const std::vector<SparseEntry<T>>& sortedEntries)
+		{
+			if (sortedEntries.empty())
+			{
+				return;
+			}
+
+			for (size_t i = 0; i < sortedEntries.size(); ++i)
+			{
+				const auto& entry = sortedEntries[i];
+				while (rowBeginIndices.size() < entry.y + 1)
+				{
+					rowBeginIndices.emplace_back(static_cast<IndexT>(xs.size()));
+				}
+
+				xs.push_back(entry.x);
+				vs.push_back(entry.v);
+			}
+
+			rowBeginIndices.emplace_back(static_cast<IndexT>(xs.size()));
+		}
+
+		const std::vector<IndexT>& getRowBeginIndices()const { return rowBeginIndices; }
+		const std::vector<IndexT>& getXs()const { return xs; }
+		const std::vector<T>& getVs()const { return vs; }
+
+	private:
+		std::vector<IndexT> rowBeginIndices;
+		std::vector<IndexT> xs;
+		std::vector<T> vs;
+	};
+}
--- test.cpp
+++ test.cpp
@@ -3,11 +3,57 @@
 
 #include "SimpleSparseMat.hpp"
 
+
 BOOST_AUTO_TEST_SUITE(aa)
 
-BOOST_AUTO_TEST_CASE(case1)
+BOOST_AUTO_TEST_CASE(init_test1)
 {
-	BOOST_CHECK(true);
+	constexpr int width = 4;
+	constexpr int height = 3;
+	const int matIn[height][width] = {
+		{0, 1, 2, 1},
+		{2, 3, 0, 5},
+		{1, 0, 4, 0},
+	};
+
+	std::vector<ssmat::SparseEntry<int>> entries;
+	for (int y = 0; y < height; ++y)
+	{
+		for (int x = 0; x < width; ++x)
+		{
+			if (matIn[y][x] != 0)
+			{
+				entries.emplace_back(x, y, matIn[y][x]);
+			}
+		}
+	}
+
+	const ssmat::SparseMat<int> sparseMat(entries);
+	const auto& columnBeginIndices = sparseMat.getRowBeginIndices();
+	const auto& xs = sparseMat.getXs();
+	const auto& vs = sparseMat.getVs();
+
+	BOOST_CHECK_EQUAL(vs.size(), xs.size());
+
+	int matOut[height][width] = {};
+	for (size_t y = 0; y + 1 < columnBeginIndices.size(); ++y)
+	{
+		const size_t rowBegin = columnBeginIndices[y];
+		const size_t rowEnd = columnBeginIndices[y + 1];
+		for (size_t i = rowBegin; i < rowEnd; ++i)
+		{
+			matOut[y][xs[i]] = vs[i];
+		}
+	}
+
+	for (size_t y = 0; y < height; ++y)
+	{
+		for (size_t x = 0; x < width; ++x)
+		{
+			BOOST_CHECK_EQUAL(matIn[y][x], matOut[y][x]);
+		}
+	}
 }
+
 
 BOOST_AUTO_TEST_SUITE_END()
